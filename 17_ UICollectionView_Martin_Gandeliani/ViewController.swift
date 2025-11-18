86972d445076db009aac8cb361874bd28cc5e993
Martin Gandeliani <martingandelyan@gmail.com>
2025-11-18 22:51:35 +0400

    Initial Commit

---
+++ 17_ UICollectionView_Martin_Gandeliani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  17_ UICollectionView_Martin_Gandeliani
+//
+//  Created by Martin on 18.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
